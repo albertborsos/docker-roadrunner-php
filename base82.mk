337065d940b30653e210aab0fce284ce5ff8f7d4
Albert Borsos <albertborsos@me.com>
2023-12-19 19:18:16 +0100

    update to build for arm64

--- base82.mk
+++ base82.mk
@@ -1,13 +1,13 @@
-alpine-base82: NAME = spacetabio/roadrunner-alpine:8.2-base
+alpine-base82: NAME = albertborsos/roadrunner-alpine:8.2-base
 alpine-base82:
-	docker build --platform=linux/amd64 -f "base/8.2/alpine.dockerfile" -t $(NAME)-$(VERSION) .
+	docker build --platform=linux/arm64 -f "base/8.2/alpine.dockerfile" -t $(NAME)-$(VERSION) .
 	@if [ "${PUSH}" == 'true' ]; then \
 	    docker push $(NAME)-$(VERSION) ; \
 	fi
 
-alpine-base-xdebug82: NAME = spacetabio/roadrunner-alpine:8.2-base-xdebug
+alpine-base-xdebug82: NAME = albertborsos/roadrunner-alpine:8.2-base-xdebug
 alpine-base-xdebug82:
-	docker build --platform=linux/amd64 -f "base-xdebug/8.2/alpine.dockerfile" -t $(NAME)-$(VERSION) .
+	docker build --platform=linux/arm64 -f "base-xdebug/8.2/alpine.dockerfile" -t $(NAME)-$(VERSION) .
 	@if [ "${PUSH}" == 'true' ]; then \
 	    docker push $(NAME)-$(VERSION) ; \
 	fi
