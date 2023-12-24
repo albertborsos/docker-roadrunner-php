16954a03ca5cf08552311fbbdc2c6f46f00582e4
Albert Borsos <albertborsos@me.com>
2023-12-24 12:05:55 +0100

    build for amd64

--- base82.mk
+++ base82.mk
@@ -1,13 +1,13 @@
 debian-base82: NAME = albertborsos/roadrunner-debian:8.2-base
 debian-base82:
-	docker build --platform=linux/arm64 -f "base/8.2/debian.dockerfile" -t $(NAME)-$(VERSION) .
+	docker build --platform=linux/amd64 -f "base/8.2/debian.dockerfile" -t $(NAME)-$(VERSION) .
 	@if [ "${PUSH}" == 'true' ]; then \
 	    docker push $(NAME)-$(VERSION) ; \
 	fi
 
 debian-base-xdebug82: NAME = albertborsos/roadrunner-debian:8.2-base-xdebug
 debian-base-xdebug82:
-	docker build --platform=linux/arm64 -f "base-xdebug/8.2/debian.dockerfile" -t $(NAME)-$(VERSION) .
+	docker build --platform=linux/amd64 -f "base-xdebug/8.2/debian.dockerfile" -t $(NAME)-$(VERSION) .
 	@if [ "${PUSH}" == 'true' ]; then \
 	    docker push $(NAME)-$(VERSION) ; \
 	fi
